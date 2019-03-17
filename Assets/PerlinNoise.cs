bce4eb4c525745dd789b70e312f4ec295b3246f2
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2019-03-17 05:28:15 -0300

    Terrain Generation added.

---
+++ Assets/PerlinNoise.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PerlinNoise : MonoBehaviour
+{
+    public int width = 256;
+    public int height = 256;
+    public float scale = 20f;
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        offsetX = Random.Range(0f, 99999f);
+        offsetY = Random.Range(0f, 99999f);
+    }
+
+    private void Update()
+    {        
+        rend.material.mainTexture = GenerateTexture();
+    }
+
+    private Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        // Generate a Perlin Noise map for the texture
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color color = CalculateColor(x, y);
+                texture.SetPixel(x, y, color);
+            }
+        }
+        texture.Apply();
+        return texture;
+    }
+
+    private Color CalculateColor(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+
+        float sample = Mathf.PerlinNoise(xCoord, yCoord);
+        return new Color(sample, sample, sample);
+    }
+}
